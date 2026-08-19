0886295e0559fd24a57d9e34f0af0ef9bed2e889
dona-capillano <mbcapillano@brc.pshs.edu.ph>
2026-08-19 19:50:42 +0800

    Update zodiacMagnesiumCapillano.py

--- q1/zodiacMagnesiumCapillano.py
+++ q1/zodiacMagnesiumCapillano.py
@@ -1,1 +1,17 @@
+zodiac_signs = [
+    "Rat (鼠 / Shǔ)",
+    "Ox (牛 / Niú)",
+    "Tiger (虎 / Hǔ)",
+    "Rabbit (兔 / Tù)",
+    "Dragon (龙 / Lóng)",
+    "Snake (蛇 / Shé)",
+    "Horse (马 / Mǎ)",
+    "Goat (羊 / Yáng)",
+    "Monkey (猴 / Hóu)",
+    "Rooster (鸡 / Jī)",
+    "Dog (狗 / Gǒu)",
+    "Pig (猪 / Zhū)"
+]
 
+baseline_year = 1900
+birth_year = int(input("Enter your birth year: "))
